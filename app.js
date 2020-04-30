2a685630a32ffbbca3661974c68d9837bb58717f
Jamie <jamieplayer@gmail.com>
2020-04-30 11:50:10 +0100

    CORS

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const PORT = 3000
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 const payload = "Payloadddddd"
 
 const options = {
